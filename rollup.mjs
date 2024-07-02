3707517c93b4854ce5bfa72b15e1bcaef34dfd2c
Tarcisio Oliveira <t@rcis.io>
2024-07-02 00:55:27 -0300

    update

---
+++ rollup.mjs
@@ -0,0 +1,47 @@
+import commonjs         from '@rollup/plugin-commonjs';
+import { nodeResolve }  from '@rollup/plugin-node-resolve';
+import replace          from '@rollup/plugin-replace';
+import terser           from '@rollup/plugin-terser';
+import { defineConfig } from 'rollup';
+import scss             from 'rollup-plugin-scss';
+import svg              from 'rollup-plugin-svg';
+
+export default function () {
+
+	return defineConfig({
+		input    : 'input.js',
+		output   : {
+			name                 : 'RollupPatternfly',
+			dir                  : 'dist',
+			file                 : 'patternfly.min.js',
+			format               : 'umd',
+			inlineDynamicImports : true,
+			globals              : {
+				'react'     : 'React',
+				'react-dom' : 'ReactDOM'
+			}
+		},
+		external : [
+			'react',
+			'react-dom'
+		],
+		plugins  : [
+			replace({
+				'process.env.NODE_ENV' : JSON.stringify('production'),
+				'preventAssignment'    : true
+			}),
+			nodeResolve(),
+			commonjs(),
+			scss({
+				fileName    : 'patternfly.min.css',
+				outputStyle : 'compressed'
+			}),
+			svg(),
+			terser({
+				format : {
+					comments : false
+				}
+			})
+		]
+	});
+};
